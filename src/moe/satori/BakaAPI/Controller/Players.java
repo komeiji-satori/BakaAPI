9bbd17f1259aa458869a77f14b2faa529324ec48
神楽坂覚々 <komeiji@satori.moe>
2018-01-18 23:18:54 +0800

    add getInfo() and sendMessage() API

--- src/moe/satori/BakaAPI/Controller/Players.java
+++ src/moe/satori/BakaAPI/Controller/Players.java
@@ -18,6 +18,30 @@ public class Players {
 		map.put("online", playerlist);
 		return map;
 	}
+	
+	public static HashMap<String, Object> sendMessage(HashMap<String, String> params) {
+		HashMap<String, Object> result = new HashMap<String,Object>();
+		String playerName = params.get("username");
+		String content = params.get("content");
+		Player player = Bukkit.getPlayer(playerName);
+		player.sendMessage(content);
+		result.put("status", 200);
+		result.put("message","ok");
+		return result;
+	}
+
+	public static HashMap<String, Object> getInfo(HashMap<String, String> params) {
+		HashMap<String, Object> result = new HashMap<>();
+		HashMap<String, Object> location = new HashMap<>();
+		String playerName = params.get("username");
+		Player player = Bukkit.getPlayer(playerName);
+		location.put("x", player.getLocation().getX());
+		location.put("y", player.getLocation().getY());
+		location.put("z", player.getLocation().getZ());
+		result.put("health", player.getHealth());
+		result.put("location", location);
+		return result;
+	}
 
 	public static HashMap<String, Object> getPlayerInventory(HashMap<String, String> params) {
 		HashMap<String, Object> result = new HashMap<>();
